1d3886adbebe04e4401d21efd8d3a99d23e9bf0b
Awesomeluc <126626596+Awesomeluc@users.noreply.github.com>
2023-03-30 01:48:16 -0400

    Update Webscraper.py

--- Webscraper.py
+++ Webscraper.py
@@ -15,3 +15,33 @@ soup = BeautifulSoup(content, 'html.parser')
 links = soup.find_all('a')
 for link in links:
     print(link.get('href'))
+import re
+
+def clean_text(text):
+    # remove HTML tags
+    text = re.sub('<[^<]+?>', '', text)
+    # remove punctuation and special characters
+    text = re.sub('[^A-Za-z0-9]+', ' ', text)
+    # convert to lowercase
+    text = text.lower()
+    # remove extra whitespace
+    text = re.sub('\s+', ' ', text).strip()
+    return text
+import csv
+
+# define the data
+data = [
+    {'title': 'Article 1', 'author': 'John Doe', 'date': '2022-04-01', 'text': 'This is the text of article 1.'},
+    {'title': 'Article 2', 'author': 'Jane Smith', 'date': '2022-04-02', 'text': 'This is the text of article 2.'},
+    {'title': 'Article 3', 'author': 'Bob Johnson', 'date': '2022-04-03', 'text': 'This is the text of article 3.'}
+]
+
+# define the headers for the CSV file
+headers = ['title', 'author', 'date', 'text']
+
+# write the data to a CSV file
+with open('data.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=headers)
+    writer.writeheader()
+    for article in data:
+        writer.writerow(article)
