69690b416374227a1e74a7a371a502cb7d39c642
Awesomeluc <126626596+Awesomeluc@users.noreply.github.com>
2023-03-30 02:09:00 -0400

    Update Webscraper.py

--- Webscraper.py
+++ Webscraper.py
@@ -6,6 +6,7 @@ url = "https://www.google.com/search?q=python"
 
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
+html_content = response.content
 
 results = []
 
@@ -13,15 +14,17 @@ for result in soup.find_all("div", class_="BNeawe iBp4i AP7Wnd"):
     link = result.find("a").get("href")
     title = result.find("a").get_text()
     description = result.find_next_sibling("div").get_text()
+    price_elements = soup.find_all('span', class_='price')
+
 
     # Clean data
     link = link.split("/url?q=")[1].split("&sa=")[0]
 
     # Append data to results
-    results.append([title, link, description])
+    results.append([title, link, description, price_elements])
 
 # Write data to CSV file
 with open("results.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Title", "Link", "Description"])
+    writer.writerow(["Title", "Link", "Description", "Price"])
     writer.writerows(results)
