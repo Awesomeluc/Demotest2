7133adff8f9e58ce6f1ebe704138101db115566e
Awesomeluc <126626596+Awesomeluc@users.noreply.github.com>
2023-04-10 01:13:36 -0400

    added compression option

--- Backup_script.ps1
+++ Backup_script.ps1
@@ -2,7 +2,7 @@
 $destinationFolder = Read-Host "Enter the path for the backup folder"
 
 # Create a new folder in the destination folder with the current date and time
-$backupFolder = Join-Path $destinationFolder (Get-Date -Format "yyyyMMdd_HHmmss")
+$backupFolder = Join-Path $destinationFolder (Get-Date -Format "MMddyyyy_HHmmss")
 try {
     New-Item -ItemType Directory -Path $backupFolder -ErrorAction Stop
 }
@@ -39,15 +39,52 @@ foreach ($folder in $userDataFolders) {
         Write-Host "Backed up $folderName to $newFolder"
     }
 }
+# Get the size of the backup folder
+$backupFolderSize = (Get-ChildItem $backupFolder -Recurse | Measure-Object -Property Length -Sum).Sum
+$backupFolderSizeReadable = "{0:N2}" -f ($backupFolderSize / 1MB)
+Write-Host "Backup folder size: $backupFolderSizeReadable MB"
 
-# Ask the user whether they want to enable compression or not
+# Calculate estimated compressed size
+$backupZipFile = "$backupFolder.zip"
+$estimatedZipFileSize = $backupFolderSize * 0.7 # Assumes 30% compression ratio
+$estimatedZipFileSizeReadable = "{0:N2}" -f ($estimatedZipFileSize / 1MB)
+Write-Host "Estimated compressed size: $estimatedZipFileSizeReadable MB"
+# Ask the user if they want to enable compression
+$compress = Read-Host "Do you want to compress the backup folder? (Y/N)"
+if ($compress -eq "Y") {
+    # Compress the backup folder
+    $backupZipFile = "$backupFolder.zip"
+    Compress-Archive -Path $backupFolder -DestinationPath $backupZipFile
+    Write-Host "Backup folder compressed to $backupZipFile"
+}
+
+
+
+
+
+
+
+<# Ask the user whether they want to enable compression or not
 $enableCompression = Read-Host "Do you want to enable compression? (Y/N)"
 if ($enableCompression.ToUpper() -eq "Y") {
     # Compress the backup folder and calculate the compressed file size
-    $compressedFile = Join-Path $destinationFolder "$(Split-Path $backupFolder -Leaf).zip"
+    $compressedFile = Join-Path $destinationFolder "$(Split-Path $backupFolder -Leaf).gz"
     try {
-        Compress-Archive -Path $backupFolder.FullName -DestinationPath $compressedFile -Force -ErrorAction Stop
-        $compressedSize = Get-ChildItem $compressedFile -Force | Select-Object -ExpandProperty Length
+        $sourceFiles = Get-ChildItem $backupFolder.FullName -Recurse -File
+        $compressedFileSize = 0
+     
+        # Create a GZipStream object and write the compressed data to the output file
+        $stream = New-Object System.IO.FileStream ($compressedFile, 'Create')
+        $gzipStream = New-Object System.IO.Compression.GZipStream $stream, ([System.IO.Compression.CompressionMode]::Compress)
+        foreach ($sourceFile in $sourceFiles) {
+            $sourceStream = $sourceFile.OpenRead()
+            $gzipStream.WriteTimeout = 60000 # Timeout in milliseconds
+            $gzipStream.Write($sourceStream, $sourceStream.Length)
+            $sourceStream.Close()
+            $compressedFileSize += $sourceFile.Length
+        }
+        $gzipStream.Close()
+        $stream.Close()
 
         # Convert the sizes to gigabytes or megabytes depending on the size
         $backupSize = $(Get-ChildItem $backupFolder -Recurse | Measure-Object -Property Length -Sum).Sum
@@ -60,12 +97,12 @@ if ($enableCompression.ToUpper() -eq "Y") {
         } else {
             $sizeUnit = "bytes"
         }
-
-        $compressedSize = [math]::Round($compressedSize / 1MB, 2)
+        
+        $compressedFileSize = [math]::Round($compressedFileSize / 1MB, 2)
 
         Write-Host "Backup completed successfully. Backup files are located in: $compressedFile"
         Write-Host "Backup folder size: $backupSize $sizeUnit"
-        Write-Host "Compressed backup file size: $compressedSize MB"
+        Write-Host "Compressed backup file size: $compressedFileSize MB"
     } catch {
         Write-Host "An error occurred during compression: $($_.Exception.Message)"
     }
@@ -86,3 +123,4 @@ if ($enableCompression.ToUpper() -eq "Y") {
     Write-Host "Backup completed successfully. Backup files are located in: $backupFolder"
     Write-Host "Backup folder size: $backupSize $sizeUnit"
 }
+#>
