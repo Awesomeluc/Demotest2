2c72af1a7acdcb64a164593db99be52b1a16f079
Awesomeluc <126626596+Awesomeluc@users.noreply.github.com>
2023-04-15 00:29:45 -0400

    Update Restore_script.ps1

--- Restore_script.ps1
+++ Restore_script.ps1
@@ -1,38 +1,15 @@
-# Set the source and destination folders
+# Set the backup file location
 $backupFile = Read-Host "Enter the path for the backup file"
-$destinationFolder = Split-Path -Parent $backupFile
 
-# Check if the backup file exists
-if (-not (Test-Path $backupFile)) {
-    Write-Host "Error: Backup file does not exist" -ForegroundColor Red
-    return
-}
-
-# Check if the destination folder exists, if not create it
-if (-not (Test-Path $destinationFolder)) {
-    try {
-        New-Item -ItemType Directory -Path $destinationFolder -ErrorAction Stop | Out-Null
-    } catch {
-        Write-Host "Error creating destination folder: $_" -ForegroundColor Red
-        return
-    }
-}
+# Set the destination folder as the parent folder of the backup file
+$destinationFolder = Split-Path $backupFile -Parent
 
 # Check if the backup file is compressed
 if ($backupFile.EndsWith(".zip")) {
-    try {
-        # Decompress the backup file
-        Expand-Archive -Path $backupFile -DestinationPath $destinationFolder -ErrorAction Stop
-    } catch {
-        Write-Host "Error decompressing backup file: $_" -ForegroundColor Red
-        return
-    }
+    # Decompress the backup file
+    Expand-Archive -Path $backupFile -DestinationPath $destinationFolder
     $backupFolder = Join-Path $destinationFolder (Get-ChildItem $destinationFolder | Select-Object -First 1).Name
 } else {
-    if (-not (Test-Path $backupFile -PathType Container)) {
-        Write-Host "Error: Backup file is not a folder" -ForegroundColor Red
-        return
-    }
     $backupFolder = $backupFile
 }
 
@@ -51,30 +28,18 @@ foreach ($folder in $userDataFolders) {
     if (Test-Path $folder) {
         $folderName = Split-Path $folder -Leaf
         $sourceFolder = Join-Path $backupFolder $folderName
-        if (Test-Path $sourceFolder -PathType Container) {
+        if (Test-Path $sourceFolder) {
             Get-ChildItem $sourceFolder -Recurse | ForEach-Object {
                 $newPath = Join-Path $folder $_.FullName.Substring($sourceFolder.Length)
                 if($_.PsIsContainer) {
-                    try {
-                        New-Item -ItemType Directory -Path $newPath -Force -ErrorAction Stop | Out-Null
-                    } catch {
-                        Write-Host "Error creating directory $newPath: $_" -ForegroundColor Red
-                        return
-                    }
+                    New-Item -ItemType Directory -Path $newPath -Force
                 } else {
-                    try {
-                        Copy-Item $_.FullName $newPath -Force -ErrorAction Stop
-                    } catch {
-                        Write-Host "Error copying file $($_.FullName) to $newPath: $_" -ForegroundColor Red
-                        return
-                    }
+                    Copy-Item $_.FullName $newPath -Force
                 }
             }
             Write-Host "Restored $folderName to $folder"
         } else {
             Write-Host "Warning: $folderName not found in backup folder" -ForegroundColor Yellow
         }
-    } else {
-        Write-Host "Warning: $folder not found on this system" -ForegroundColor Yellow
     }
 }
